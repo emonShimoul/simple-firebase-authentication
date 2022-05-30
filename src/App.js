375414343ed690c25e6606c330e3ab6aa154cb12
emonShimoul <emonshimoul@gmail.com>
2022-05-31 00:23:58 +0600

    display user name on the UI from google account

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useState } from "react";
 import './App.css';
 
 import initializeAuthentication from "./Firebase/firebase.initialize";
 initializeAuthentication();
 
 function App() {
+  const [user, setUser] = useState({});
   const provider = new GoogleAuthProvider();
   const auth = getAuth();
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
     .then(result => {
-      const user = result.user;
-      console.log(user);
+      const {displayName, email, photoURL} = result.user;
+      const loggedInUser = {
+        name: displayName,
+        email: email,
+        photo: photoURL
+      };
+      setUser(loggedInUser);
     })
   }
   return (
     <div className="App">
       <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      <br />
+      {
+        user.email && <div>
+          <h2>Welcome {user.name}</h2>
+        </div>
+      }
     </div>
   );
 }

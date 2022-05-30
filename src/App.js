83e76b86033e5a256d323169f14217a80a8b2fd8
emonShimoul <emonshimoul@gmail.com>
2022-05-31 00:17:09 +0600

    setup firebase google authentication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import './App.css';
 
+import initializeAuthentication from "./Firebase/firebase.initialize";
+initializeAuthentication();
+
 function App() {
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+    .then(result => {
+      const user = result.user;
+      console.log(user);
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
     </div>
   );
 }

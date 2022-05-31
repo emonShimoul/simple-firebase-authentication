5b82007a6e35b7c775534f9b5a795bdf66e9c17d
emonShimoul <emonshimoul@gmail.com>
2022-05-31 21:34:51 +0600

    add github authentication

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 import { useState } from "react";
 import './App.css';
 
@@ -7,10 +7,13 @@ initializeAuthentication();
 
 function App() {
   const [user, setUser] = useState({});
-  const provider = new GoogleAuthProvider();
+
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+
   const auth = getAuth();
   const handleGoogleSignIn = () => {
-    signInWithPopup(auth, provider)
+    signInWithPopup(auth, googleProvider)
     .then(result => {
       const {displayName, email, photoURL} = result.user;
       const loggedInUser = {
@@ -24,9 +27,17 @@ function App() {
       console.log(error.message);
     })
   }
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, githubProvider)
+    .then(result => {
+      const user = result.user;
+      console.log(user);
+    })
+  }
   return (
     <div className="App">
       <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      <button onClick={handleGithubSignIn}>Github Sign In</button>
       <br />
       {
         user.email && <div>
